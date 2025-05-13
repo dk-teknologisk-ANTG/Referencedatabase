92eb7cffd4023b8cd25f605c7ef60b81e6321744
dk-teknologisk-ANTG <antg@teknologisk.dk>
2025-05-13 11:48:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from docx.shared import RGBColor, Pt
 import openpyxl
 import datetime
 
+os.environ['http_proxy'] = 'http://squid18.localdom.net:3128'
+os.environ['https_proxy'] = 'http://squid18.localdom.net:3128'
+
 url: str = "https://etxhbhpjqoaoowfoscob.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0eGhiaHBqcW9hb293Zm9zY29iIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDcwNTAzNiwiZXhwIjoyMDYwMjgxMDM2fQ.xJ7aC9TrQU-qbdf6KBB9D4FmyANf3NSvEHhpO4-lUEQ"
 
